c798b5dbd434488d2936296cd19f3899d3591d1f
andrewjlowery <andrew.lowery@cognitoiq.com>
2020-09-06 12:51:53 +0100

    test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from git import Repo
 import git
 import github
-import pathlib
+from pathlib import Path
 from subprocess import Popen, PIPE
 import requests
 import os
@@ -11,6 +11,13 @@ if "INPUT_TERRAFORM_VERSION" in os.environ:
     tf_ver = os.getenv("INPUT_TERRAFORM_VERSION")
 else:
     tf_ver = ""
+
+if "GITHUB_TOKEN" in os.environ:
+    github_token = os.getenv("GITHUB_TOKEN")
+else:
+    github_token = ""
+
+
 
 def terraform_check_fmt():
     p = Popen(["./terraform", "fmt", "-check", "-recursive"], stdout=PIPE, stderr=PIPE)
@@ -22,30 +29,30 @@ def terraform_fmt():
     print(p.communicate())
     return p.returncode
 
-# def create_branch(repo, branch):
-#     repo.git.branch(branch)
+def create_branch(repo, branch):
+    repo.git.branch(branch)
 
-# def add_commit_to_branch(branch, message):
-#     repo.git.checkout(branch)
-#     repo.git.add(".")
-#     repo.index.commit(message)
+def add_commit_to_branch(branch, message):
+    repo.git.checkout(branch)
+    repo.git.add(".")
+    repo.index.commit(message)
 
-# def push_to_origin(branch):
-#     origin.push(branch)
+def push_to_origin(branch):
+    origin.push(branch)
 
-# def create_pull_request(api_token, from_branch, into_branch):
-#     g = github.Github(api_token)
-#     repo = g.get_repo("lowery-me-uk/terraform-dns")
-#     repo.create_pull(title="fixing fmt", body='body', head=from_branch, base=into_branch)
+def create_pull_request(api_token, from_branch, into_branch):
+    g = github.Github(api_token)
+    repo = g.get_repo("lowery-me-uk/terraform-dns")
+    repo.create_pull(title="fixing fmt", body='body', head=from_branch, base=into_branch)
 
 if __name__ == "__main__":
-    # repo = clone_repo('https://github.com/lowery-me-uk/terraform-dns')
+    branch = "test"
     tf.install(tf_ver)
+    repo = Repo(Path.cwd())
     if terraform_check_fmt() != 0:
-        print('yup')
-        # origin = repo.remote()
-        # create_branch(repo, branch)
-        # terraform_fmt()
-        # add_commit_to_branch(branch,'fixing fmt')
-        # push_to_origin(branch)
-        # create_pull_request(token, branch, 'master')
+        origin = repo.remote()
+        create_branch(repo, branch)
+        terraform_fmt()
+        add_commit_to_branch(branch,'fixing fmt')
+        push_to_origin(branch)
+        create_pull_request(github_token, branch, 'master')
